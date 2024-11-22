fb4054d53bebe3fdc22cc2683e164f01c746f39a
Ekaterina Merkulova <kate_merek@mail.ru>
2024-11-22 13:39:57 +0300

    v 2.7.1

--- Clients/src/main/java/com/github/katemerek/clients/ClientsApplication.java
+++ Clients/src/main/java/com/github/katemerek/clients/ClientsApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
     @SpringBootApplication
     public class ClientsApplication {
-
         public static void main(String[] args) {
             Application.launch(JavaFxApplication.class, args);
         }
-
     }
