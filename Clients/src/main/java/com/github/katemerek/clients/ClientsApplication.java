3e39544a1c1b1d67f53c52fb8004ce97e75719a7
Ekaterina Merkulova <kate_merek@mail.ru>
2024-11-22 12:15:40 +0300

    v 2.7. Привела модули в порядок

---
+++ Clients/src/main/java/com/github/katemerek/clients/ClientsApplication.java
@@ -0,0 +1,14 @@
+package com.github.katemerek.clients;
+
+import com.github.katemerek.clients.view.JavaFxApplication;
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class ClientsApplication {
+
+        public static void main(String[] args) {
+            Application.launch(JavaFxApplication.class, args);
+        }
+
+    }
---
+++ Clients/src/main/java/com/github/katemerek/clients/security/PersonDetails.java
@@ -0,0 +1,52 @@
+package com.github.katemerek.clients.security;
+
+
+import com.github.katemerek.clients.models.Person;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class PersonDetails implements UserDetails {
+    private final Person person;
+
+    public PersonDetails(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return person.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
